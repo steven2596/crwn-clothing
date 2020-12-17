0c620418844425f7975416f73adf15ee8db5f0da
Steven <aungkaungmyat2596@gmail.com>
2020-12-17 14:54:48 +0630

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const Hatpage = () => (
+  <div>
+    <h1>Welcome to Hats Section!</h1>
+  </div>
+);
+
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={Hatpage} />
+
+
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.style.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`} >
-        <div style=
-            {{ backgroundImage: `url(${imageUrl})` }}
-            className="background-image" />
-        <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <p className="subtitle">SHOP NOW</p>
-        </div>
-    </div >
-)
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
+    console.log(match.url);
+    return (
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)} >
+            <div style=
+                {{ backgroundImage: `url(${imageUrl})` }}
+                className="background-image" />
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <p className="subtitle">SHOP NOW</p>
+            </div>
+        </div >
+    )
+}
+export default withRouter(MenuItem);
 
-export default MenuItem;
-
+// match.url+linkUrl = '/hats'
